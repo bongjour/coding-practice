efc98079365fa53c3f0f5a7def01c6b67d4666b0
dante(성기봉) <inomy@coupang.com>
2016-12-13 23:28:25 +0900

    한문제 더

---
+++ src/main/java/io/jourbong/code/practice/codingdojang/EightCounter.java
@@ -0,0 +1,32 @@
+package io.jourbong.code.practice.codingdojang;
+
+/**
+ * 1부터 10,000까지 8이라는 숫자가 총 몇번 나오는가?
+ * 8이 포함되어 있는 숫자의 갯수를 카운팅 하는 것이 아니라 8이라는 숫자를 모두 카운팅 해야 한다.
+ * (※ 예를들어 8808은 3, 8888은 4로 카운팅 해야 함)
+ */
+public class EightCounter {
+
+	private static int totalCount = 0;
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 10000; i++) {
+			countEight(i);
+		}
+
+		System.out.println(totalCount);
+	}
+
+	public static void countEight(int maxNumber) {
+
+		if (maxNumber % 10 == 8) {
+			totalCount++;
+		}
+
+		if (maxNumber > 10) {
+			countEight(maxNumber / 10);
+		}
+
+	}
+}
